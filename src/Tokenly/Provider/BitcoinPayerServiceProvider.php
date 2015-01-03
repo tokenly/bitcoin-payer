7c237af76a35d5381a92fc434e5cc569164de29d
Devon Weller <dweller@devonweller.com>
2015-01-03 11:52:22 -0600

    updated dependencies

---
+++ src/Tokenly/Provider/BitcoinPayerServiceProvider.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace Tokenly\BitcoinAddressLib;
+
+
+use Exception;
+use Illuminate\Support\ServiceProvider;
+use Nbobtc\Bitcoind\Client;
+use Tokenly\BitcoinPayer\BitcoinPayer;
+
+/*
+* BitcoinPayerServiceProvider
+*/
+class BitcoinPayerServiceProvider extends ServiceProvider
+{
+
+    public function boot()
+    {
+    }
+
+    /**
+     * Register the service provider.
+     *
+     * @return void
+     */
+    public function register()
+    {
+        $this->package('tokenly/bitcoin-payer', 'bitcoin-payer', __DIR__.'/../../');
+
+        $this->app->bind('Tokenly\BitcoinPayer\BitcoinPayer', function($app) {
+            $xcpd_client = $app->make('Tokenly\XCPDClient\Client');
+            $config = $app['config']['bitcoin::bitcoin'];
+            $connection_string = "{$config['scheme']}://{$config['rpcUser']}:{$config['rpcPassword']}@{$config['host']}:{$config['port']}";
+            $bitcoin_client = new Client($connection_string);
+            $sender = new BitcoinPayer($xcpd_client, $bitcoin_client);
+            return $sender;
+        });
+    }
+
+}
+
