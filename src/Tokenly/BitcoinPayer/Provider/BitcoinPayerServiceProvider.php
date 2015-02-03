ce90a4816aa075c11c3de4502701116e211da5e8
Devon Weller <dweller@devonweller.com>
2015-02-02 22:15:58 -0600

    updated to support laravel 5 config

--- src/Tokenly/BitcoinPayer/Provider/BitcoinPayerServiceProvider.php
+++ src/Tokenly/BitcoinPayer/Provider/BitcoinPayerServiceProvider.php
@@ -4,6 +4,7 @@ namespace Tokenly\BitcoinPayer\Provider;
 
 
 use Exception;
+use Illuminate\Support\Facades\Config;
 use Illuminate\Support\ServiceProvider;
 use Tokenly\BitcoinPayer\BitcoinPayer;
 
@@ -24,11 +25,15 @@ class BitcoinPayerServiceProvider extends ServiceProvider
      */
     public function register()
     {
-        $this->package('tokenly/bitcoin-payer', 'bitcoin-payer', __DIR__.'/../../');
+        $this->bindConfig();
 
         $this->app->bind('Nbobtc\Bitcoind\Bitcoind', function($app) {
-            $config = $app['config']['bitcoin'];
-            $connection_string = "{$config['scheme']}://{$config['rpcUser']}:{$config['rpcPassword']}@{$config['host']}:{$config['port']}";
+
+            $url_pieces = parse_url(Config::get('bitcoin-payer.connection_string'));
+            $rpc_user = Config::get('bitcoin-payer.rpc_user');
+            $rpc_password = Config::get('bitcoin-payer.rpc_password');
+            $url_pieces = parse_url(Config::get('bitcoin-payer.connection_string'));
+            $connection_string = "{$url_pieces['scheme']}://{$rpc_user}:{$rpc_password}@{$url_pieces['host']}:{$url_pieces['port']}";
             $bitcoin_client = new Client($connection_string);
             $bitcoind = new Bitcoind($bitcoin_client);
             return $bitcoind;
@@ -42,5 +47,21 @@ class BitcoinPayerServiceProvider extends ServiceProvider
         });
     }
 
+    protected function bindConfig()
+    {
+
+
+        // simple config
+        $config = [
+            'bitcoin-payer.connection_string' => env('NATIVE_CONNECTION_STRING', 'http://localhost:8332'),
+            'bitcoin-payer.rpc_user'          => env('NATIVE_RPC_USER', null),
+            'bitcoin-payer.rpc_password'      => env('NATIVE_RPC_PASSWORD', null),
+        ];
+
+        // set the laravel config
+        Config::set($config);
+    }
+
 }
+
 
